124b2fdbdb248c04a2cabcf2fa35176f04ad47e9
DietCokeNoSoul <gtalhy@163.com>
2025-08-30 14:44:41 +0800

    Exception handle

---
+++ web/src/main/java/com/yep/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.yep.web.exception;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.yep.web.pojo.Result;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 处理所有异常
+    @ExceptionHandler
+    public Result handleException(Exception e) {
+        log.error("服务器异常", e);
+        return Result.error("服务器异常，请联系管理员");
+    }
+
+    // 处理数据库唯一键冲突异常，优先进入该函数
+    @ExceptionHandler
+    public Result handleDuplicateKeyException(DuplicateKeyException e) {
+        log.error("数据库唯一键冲突异常", e);
+        String msg = e.getMessage();
+        int i = msg.indexOf("Duplicate entry");
+        String errMsg = msg.substring(i);
+        String[] arr = errMsg.split(" ");
+        return Result.error(arr[2] + "已存在");
+    }
+}
